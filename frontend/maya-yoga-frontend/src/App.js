f4f8cfe3722e93e31fb9b4f690cde04a77e931dc
Alberto Duarte <betodute@gmail.com>
2024-10-20 02:11:21 -0400

    build folder added

---
+++ frontend/maya-yoga-frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1>Maya Yoga - Horario</h1>
+          <p>November 2024:</p>
+          <ul className='class-schedule'>
+            <li> Slow Vinyasa con Athenas - Lunes, Miercoles y Viernes a las 9am </li>
+            <li> Yin Yoga con Soco - Martes y Jueves a las 9am </li>
+            <li> Mindful Vinayasa con Maya - Lunes, Miercoles, y Viernes a las 6pm </li>
+            <li>  </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

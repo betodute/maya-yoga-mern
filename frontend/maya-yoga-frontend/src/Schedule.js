66c4be8aadc7457ef9435db69090def73fd5f432
Alberto Duarte <betodute@gmail.com>
2024-10-21 02:17:57 -0400

    padding over margins for more control over space between nodes

--- frontend/maya-yoga-frontend/src/Schedule.js
+++ frontend/maya-yoga-frontend/src/Schedule.js
@@ -5,35 +5,31 @@ export const Schedule = () => {
   return (
     <div className="schedule-wrapper">
       <div className="day-column dark-grey">
-        <h2 className="green">Lunes</h2>
+        <h2 className="blue">Lunes</h2>
         <p className="am-class">Original Hot Yoga<br />c/ Beto 7am</p>
         <p className="am-class">Slow Vinyasa<br />c/ Athenas 9am</p>
         <p className="pm-class">Mindful Vinyasa<br />c/ Maya 6pm</p>
       </div>
-      <div className="spacer"></div>
-      <div className="day-column">
+      <div className="day-column test-size">
         <h2 className="blue">Martes</h2>
         <p className="filler"></p> {/* Blank slot for 7am */}
         <p className="am-class">Yin Yoga<br />c/ Soco 9am</p>
         <p className="pm-class">Slow Vinyasa<br />c/ Athenas 6pm</p>
       </div>
-      <div className="spacer"></div>
       <div className="day-column dark-grey">
-        <h2 className="green">Miércoles</h2>
-        <p className="am-class">Hot Yin<br />c/ Beto 7am</p>
+        <h2 className="blue">Miércoles</h2>
+        <p className="am-class">Hot Yin Yoga<br />c/ Beto 7am</p>
         <p className="am-class">Slow Vinyasa<br />c/ Athenas 9am</p>
         <p className="pm-class">Mindful Vinyasa<br />c/ Maya 6pm</p>
       </div>
-      <div className="spacer"></div>
       <div className="day-column">
         <h2 className="blue">Jueves</h2>
         <p className="filler"></p> {/* Blank slot for 7am */}
         <p className="am-class">Yin Yoga<br />c/ Soco 9am</p>
         <p className="pm-class">Slow Vinyasa<br />c/ Athenas 6pm</p>
       </div>
-      <div className="spacer"></div>
       <div className="day-column dark-grey">
-        <h2 className="green">Viernes</h2>
+        <h2 className="blue">Viernes</h2>
         <p className="am-class">Original Hot Yoga<br />c/ Beto 7am</p>
         <p className="am-class">Slow Vinyasa<br />c/ Athenas 9am</p>
         <p className="pm-class">Mindful Vinyasa<br />c/ Maya 6pm</p>

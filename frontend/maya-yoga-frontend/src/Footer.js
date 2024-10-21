0cadddd442d9501bafe8fa0503ea5081f9d86b18
Alberto Duarte <betodute@gmail.com>
2024-10-21 02:35:49 -0400

    typeface and spacing

--- frontend/maya-yoga-frontend/src/Footer.js
+++ frontend/maya-yoga-frontend/src/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css';
 export const Footer = () => {
     return(
         <div className='footer-wrapper'>
-            <p> Calle Cuarta 1848  </p>
-            <p> +52 646 117 4527 </p>
+            <p className='address'> Calle Cuarta 1848  </p>
+            <p className='phone'> +52 646 117 4527 </p>
         </div>
     )
 }
